971fceacbe625e1d96e89ef6140f988ad7a32e85
MaximePolese <143593018+MaximePolese@users.noreply.github.com>
2023-09-04 15:16:46 +0200

    Avancement sur le footer

--- footer.php
+++ footer.php
@@ -1,6 +1,19 @@
 </main>
-<footer>
-FOOTER
+    <footer>
+        <div class="d-flex flex-row">
+            <div class="d-flex py-2" style="background-color:  #7FD8BE;">
+                <div class="px-2">© 2023 Copyright: Le Campus Numérique</div>
+                <a class="px-2" href="#">Conditions générales</a>
+                <a class="px-2" href="#">Mentions légales</a>
+                <a class="px-2" href="#">Politique de confidentialité</a>
+                <a class="px-2" href="#">Gestion des cookies</a>
+            </div>
+            <div class="d-flex flex-row-reverse">
+                <a href="http://www.instagram.fr"><img src="icons/instagram.png" alt="instagram" width="30"></a>
+                <a href="http://www.tiktok.fr"><img src="icons/tiktok.png" alt="tiktok" width="30"></a>
+                <a href="http://www.facebook.fr"><img src="icons/facebook.png" alt="facebook" width="30"></a>
+            </div>
+        </div>
 </footer>
 </body>
 </html>
--- header.php
+++ header.php
@@ -14,6 +14,7 @@
 <body>
 <header>
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container">
     <div class="container-fluid">
         <a class="navbar-brand" href="#">Rando Shop</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -29,17 +30,14 @@
                 </li>
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Dropdown
+                       Produits
                     </a>
                     <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="#">Action</a></li>
-                        <li><a class="dropdown-item" href="#">Another action</a></li>
+                        <li><a class="dropdown-item" href="#">Sacs</a></li>
+                        <li><a class="dropdown-item" href="#">Tentes</a></li>
                         <li><hr class="dropdown-divider"></li>
                         <li><a class="dropdown-item" href="#">Something else here</a></li>
                     </ul>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link disabled" aria-disabled="true">Disabled</a>
                 </li>
             </ul>
             <form class="d-flex" role="search">
@@ -47,6 +45,7 @@
                 <button class="btn btn-outline-success" type="submit">Search</button>
             </form>
         </div>
+    </div>
     </div>
 </nav>
 </header>
