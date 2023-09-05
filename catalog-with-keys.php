db6558e1884095d0974110bf495aa514e369a8ff
Maxime Polese <maxime.polese@le-campus-numerique.fr>
2023-09-05 15:55:29 +0200

    Mise en page de l'affichage catalog-with-keys

--- catalog-with-keys.php
+++ catalog-with-keys.php
@@ -2,16 +2,18 @@
 
 $Sacs = [
     "name" => "Deuter trail 30",
-    "price" => 14500,
+    "price" => 145,
     "weight" => 1120,
     "discount" => 10,
     "picture_url" => "https://dk0fkjygbn9vu.cloudfront.net/cache-buster-11693445103/deuter/mediaroom/product-images/backpacks/hiking-backpacks/159244/image-thumb__159244__deuter_lightbox-img/3440723-3253-Trail30_wave_ivy-D-00.png"
 ];
 
 print_r($Sacs);
+echo "<br>";
+echo "<br>";
 
-echo $Sacs["name"] . " ";
-echo $Sacs["price"] . " ";
-echo $Sacs["weight"] . " ";
-echo $Sacs["discount"] . " ";
-echo $Sacs["picture_url"] . " ";
+echo $Sacs["name"] . "<br>";
+echo $Sacs["price"] . "<br>";
+echo $Sacs["weight"] . "<br>";
+echo $Sacs["discount"] . "<br>";
+echo $Sacs["picture_url"] . "<br>";
