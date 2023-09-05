735e5add1a61655e0c9138c7e0f9d595573f7294
Maxime Polese <maxime.polese@le-campus-numerique.fr>
2023-09-05 15:53:31 +0200

    Boucles pour single-catalog OK

--- simple-catalog.php
+++ simple-catalog.php
@@ -2,14 +2,44 @@
 
 $products = ["Sacs", "Tentes", "Matelas", "Sacs de couchage", "Bâtons"];
 
+//afficher le tableau
 print_r($products);
+echo "<br>";
+echo "<br>";
 
+//Classer dans l'ordre croissant alphabétique
 echo sort($products);
-foreach ($products as $val) {
-    echo $val . " ";
-}
+echo "<br>";
 
-echo $products[0];
+//boucle while
+$i = 0;
+while ($i <= 4) {
+    echo $products[$i] . "<br>";
+    $i++;
+}
+echo "<br>";
+
+//boucle do while
+$i = 0;
+do {
+    echo $products[$i] . "<br>";
+    $i++;
+} while ($i <= 4);
+echo "<br>";
+
+//boucle for
+for ($i = 0; $i < count($products); $i++) {
+    echo $products[$i] . "<br>";
+}
+echo "<br>";
+
+//boucle foreach
+foreach ($products as $product) {
+    echo $product . "<br>";
+}
+echo "<br>";
+
+echo $products[0] . "<br>";
 echo $products[4];
 
 
