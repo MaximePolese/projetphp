e5a331472dc623a6741a408480e4bc507c730f71
MaximePolese <143593018+MaximePolese@users.noreply.github.com>
2023-09-07 11:33:38 +0200

    Modif texte en gras

--- item.php
+++ item.php
@@ -14,16 +14,18 @@ include 'header.php';
 <?php foreach ($sacs as $sac) { ?>
 
     <section class="container">
-        <article class="product d-flex justify-content-around border border-warning  border-3 my-2">
+        <article class="product d-flex justify-content-around border border-warning border-3 my-2">
             <h2 class="productName p-2"><?php echo $sac["name"] ?></h2>
-            <p class="productDescription p-2">Description : <?php echo $sac["description"] ?></p>
+            <p class="productDescription p-2"><span>Description : </span><?php echo $sac["description"] ?></p>
             <img class="productPic align-self-center p-2" src="<?php echo $sac["picture_url"] ?>" alt="">
             <div class="d-flex flex-column justify-content-end">
-                <p>Poids : <?php echo $sac["weight"] ?> gr</p>
-                <p>Prix HT : <?php echo formatPrice(priceExcludingVAT($sac["price"], $sac["vat"])); ?></p>
-                <p>Prix TTC : <?php echo formatPrice($sac["price"]); ?></p>
-                <p>Remise : <?php echo $sac["discount"] ?> %</p>
-                <p>Prix remisé : <?php echo formatPrice(discountedPrice($sac["price"], $sac["discount"])); ?></p>
+                <p><span>Poids : </span><?php echo $sac["weight"] ?> gr</p>
+                <p><span>Prix HT : </span><?php echo formatPrice(priceExcludingVAT($sac["price"], $sac["vat"])); ?></p>
+                <p><span>Prix TTC : </span><?php echo formatPrice($sac["price"]); ?></p>
+                <p><span>Remise : </span><?php echo $sac["discount"] ?> %</p>
+                <p>
+                    <span>Prix remisé : </span><?php echo formatPrice(discountedPrice($sac["price"], $sac["discount"])); ?>
+                </p>
             </div>
         </article>
     </section>
