b10c95187c43179b150d22b67a83190ba57022bc
MaximePolese <143593018+MaximePolese@users.noreply.github.com>
2023-09-07 11:11:28 +0200

    Descriptions transférées dans le catalogue

--- item.php
+++ item.php
@@ -1,6 +1,5 @@
 <?php
 include 'my-functions.php';
-$description = "La surface de contact réduite et les grands canaux d’aération du système dorsal Airstripes de deuter, combinés aux bretelles et aux stabilisateurs de hanche perforés (avec poches), assurent un flux d’air maximal et une répartition uniforme du poids pour un maintien parfait et sécurisé sur tous les terrains. La construction plate du rabat offre un bon dégagement pour la tête, même avec un casque. Les boucles sur les bretelles permettent de garder les mousquetons de via ferrata à portée de main. Et grâce à une grande ouverture frontale zippée et à des poches latérales, les équipements et le matériel sont toujours faciles d’accès.";
 include 'simple-catalog.php';
 //include 'catalog-with-keys.php';
 include 'multidimensional-catalog.php';
@@ -17,7 +16,7 @@ include 'header.php';
     <section class="container">
         <article class="product d-flex justify-content-around border border-warning  border-3 my-2">
             <h2 class="productName p-2"><?php echo $sac["name"] ?></h2>
-            <p class="productDescription p-2">Description : <?php echo $description ?></p>
+            <p class="productDescription p-2">Description : <?php echo $sac["description"] ?></p>
             <img class="productPic align-self-center p-2" src="<?php echo $sac["picture_url"] ?>" alt="">
             <div class="d-flex flex-column justify-content-end">
                 <p>Poids : <?php echo $sac["weight"] ?> gr</p>
--- multidimensional-catalog.php
+++ multidimensional-catalog.php
@@ -3,6 +3,7 @@
 $sacs = [
     "trail 30" => [
         "name" => "Deuter trail 30",
+        "description" => "La surface de contact réduite et les grands canaux d’aération du système dorsal Airstripes de deuter, combinés aux bretelles et aux stabilisateurs de hanche perforés (avec poches), assurent un flux d’air maximal et une répartition uniforme du poids pour un maintien parfait et sécurisé sur tous les terrains. La construction plate du rabat offre un bon dégagement pour la tête, même avec un casque. Les boucles sur les bretelles permettent de garder les mousquetons de via ferrata à portée de main. Et grâce à une grande ouverture frontale zippée et à des poches latérales, les équipements et le matériel sont toujours faciles d’accès. ",
         "vat" => 20,
         "price" => 14500,
         "weight" => 1120,
@@ -11,6 +12,7 @@ $sacs = [
     ],
     "FUTURA 32" => [
         "name" => "Deuter FUTURA 32",
+        "description" => "Le Futura a été conçu pour les randonnées d'une journée ou de plusieurs jours, où le confort de port et la maniabilité sont primordiaux. La nouvelle maille du système dorsal Aircomfort assure une ventilation maximale. Cela signifie jusqu'à 25 % de transpiration en moins et donc une meilleure performance. Les bretelles mobiles ActiveFit et les ailerons de hanche ergonomiques (avec réglage par traction vers l'avant) assurent également un confort de port agréable. En outre, le compartiment inférieur séparé offre des possibilités d'organisation supplémentaires dans le sac à dos.",
         "vat" => 20,
         "price" => 16500,
         "weight" => 1440,
@@ -19,6 +21,7 @@ $sacs = [
     ],
     "AC Lite 14 SL" => [
         "name" => "Deuter AC Lite 14 SL",
+        "description" => "Le nouveau AC Lite est le sac parfait pour les randonnées d’une journée. Ce sac à dos est encore plus léger que son modèle précédent et dispose d’une ventilation maximale grâce au système dorsal Aircomfort. La grande ouverture frontale à fermeture éclair offre une excellente vue d'ensemble du sac et de son contenu. Le smartphone ou les en-cas peuvent être rapidement rangés dans la poche latérale élastique aérée. Il est également possible d'accrocher un casque sur les attaches à l'extérieur du sac à dos.",
         "vat" => 20,
         "price" => 10000,
         "weight" => 860,
@@ -27,6 +30,7 @@ $sacs = [
     ],
     "Futura Pro 42 EL" => [
         "name" => "Deuter Futura Pro 42 EL",
+        "description" => "Le sac à dos de randonnée léger et fonctionnel Futura EL est destiné aux personnes particulièrement grandes (à partir de 185 cm) et constitue le compagnon idéal pour les randonnées sportives d'une journée : le système de dos Aircomfort est super confortable et offre une ventilation maximale au niveau du dos grâce à la nouvelle maille. Ce qui veut dire : jusqu'à 25 % de transpiration en moins et donc de meilleures performances. Si les vêtements sont humides, ils peuvent sécher rapidement dans la poche avant élastique et perméable à l'air pendant la randonnée. ",
         "vat" => 20,
         "price" => 22500,
         "weight" => 1700,
